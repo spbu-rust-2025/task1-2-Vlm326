4dcb5840f7f30d10775a22f9dab55e62ae74c077
Vladislav <morozvv75@gmail.com>
2025-10-25 21:06:58 +0300

    feat: implemented sum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
+use std::io;
 
+fn main() {
+    let mut sum = 0;
+
+    loop {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        let input = input.trim();
+
+        if input == "-1" {
+            break;
+        }
+
+        let number: u32 = match input.parse() {
+            Ok(n) if n > 0 => n,
+            _ => {
+                println!("NaN");
+                return;
+            }
+        };
+
+        sum += number;
+    }
+
+    println!("{}", sum);
 }
